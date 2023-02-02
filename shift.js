8a36d9c4c2a287db587c32e78a7498da95b07dc8
Joaotds <109186798+joaotds@users.noreply.github.com>
2023-02-02 08:31:35 -0300

    Create shift.js

---
+++ shift.js
@@ -0,0 +1,8 @@
+const variavel = [1, 2, 3, 4];
+
+const valorExtraido = variavel.shift();
+
+console.log(valorExtraido)
+console.log("\n", variavel)
+
+//shift é igual o pop, mas ao invés do ultimo, pega o primeiro
